d3b659cb17e985275d2d7a2861b7117fc3dcbc0a
Thomas Herzog <Tomrule007@gmail.com>
2019-07-21 14:20:05 -0700

    arcade 67 complete

--- arcade/67-houseNumbersSum/houseNumbersSum.js
+++ arcade/67-houseNumbersSum/houseNumbersSum.js
@@ -1,3 +1,8 @@
-function houseNumbersSum(inputArray) {
-
+//return sum of all numbers to the left of the first zero
+function houseNumbersSum(houseNumbers) {
+    let sum = 0;
+    for (let i = 0; houseNumbers[i] != 0 && i < houseNumbers.length; i++) {
+        sum += houseNumbers[i];
+    }
+    return sum
 }
