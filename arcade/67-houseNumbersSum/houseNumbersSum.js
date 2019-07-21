4beac8d1dade1d6bd787430834df60255bf505c4
Thomas Herzog <Tomrule007@gmail.com>
2019-07-21 14:28:17 -0700

    arcade 67 added missing semicolon on return line

--- arcade/67-houseNumbersSum/houseNumbersSum.js
+++ arcade/67-houseNumbersSum/houseNumbersSum.js
@@ -4,5 +4,5 @@ function houseNumbersSum(houseNumbers) {
     for (let i = 0; houseNumbers[i] != 0 && i < houseNumbers.length; i++) {
         sum += houseNumbers[i];
     }
-    return sum
+    return sum;
 }
