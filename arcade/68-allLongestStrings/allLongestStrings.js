41d1a744853719d907c83f7beca1159f2b205779
Thomas Herzog <Tomrule007@gmail.com>
2019-07-22 14:45:42 -0700

    arcade 68 complete

--- arcade/68-allLongestStrings/allLongestStrings.js
+++ arcade/68-allLongestStrings/allLongestStrings.js
@@ -1,3 +1,11 @@
 function allLongestStrings(inputArray) {
+    const stringSizeHash = inputArray.reduce((acc, cur) => {
+        acc[cur.length] = (acc[cur.length] || []).concat(cur);
+        return acc;
+    }, {});
+
+    const longest = Math.max(...Object.keys(stringSizeHash));
+
+    return stringSizeHash[longest];
 
 }
